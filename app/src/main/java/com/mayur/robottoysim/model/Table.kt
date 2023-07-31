18e6e7859b27f16a600ba0ee17138040afef40d1
mayur_patil <mayur.patil@reposenergy.com>
2023-07-31 16:46:18 +0530

    removed unnecessary import

--- app/src/main/java/com/mayur/robottoysim/model/Table.kt
+++ app/src/main/java/com/mayur/robottoysim/model/Table.kt
@@ -1,7 +1,5 @@
 package com.mayur.robottoysim.model
 
-import com.mayur.robottoysim.model.Position
-
 data class Table(val width: Int = 4, val height: Int = 4) {
 
     fun withinBoundaries(position: Position) = position.x in 0..width && position.y in 0..height
